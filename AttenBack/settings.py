40b3b1252c1f043e1bb1457c18bbd73503bc9729
imran76327 <is371030@gmail.com>
2025-04-24 10:49:16 +0530

    gfrg

--- AttenBack/settings.py
+++ AttenBack/settings.py
@@ -126,8 +126,8 @@ DATABASES = {
         'NAME': 'attendance_management',      # Replace with your database name
         'USER': 'root',      # Replace with your database user
         'PASSWORD': 'Mysql_MH-047319',  # Replace with your database password
-        # 'HOST': '3.109.160.130',       # IP FOR TEST
-        'HOST': '127.0.0.1',       # IP FOR LOCAL VM
+        'HOST': '3.109.160.130',       # IP FOR TEST
+        # 'HOST': '127.0.0.1',       # IP FOR LOCAL VM
         'PORT': '3306',            
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
